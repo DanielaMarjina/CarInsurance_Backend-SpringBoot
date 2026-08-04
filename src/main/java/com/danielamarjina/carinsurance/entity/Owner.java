cd888f6a551acfb4cb9bfb01b8694fa7fb6b3b1c
DanielaMarjina <daniela.marjina@student.usv.ro>
2026-08-04 22:58:32 +0300

    feat(owners):Added owner DTOs

--- src/main/java/com/danielamarjina/carinsurance/entity/Owner.java
+++ src/main/java/com/danielamarjina/carinsurance/entity/Owner.java
@@ -20,16 +20,12 @@ public class Owner {
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
-    @NotBlank
-    @Size(max = 100)
     @Column(nullable = false, length = 100)
     private String name;
 
-    @Past
     @Column(nullable = false)
     private LocalDate birthdate;
 
-    @Min(1900)
     @Column(name="year_of_driver_license",nullable = false)
     private Integer yearOfDriverLicense;
 
@@ -37,7 +33,6 @@ public class Owner {
     @Column(name = "driver_license_cat")
     private DriverLicenseCategory driverLicenseCategory;
 
-    @Email
     @Column(unique = true, length = 150)
     private String email;
 
