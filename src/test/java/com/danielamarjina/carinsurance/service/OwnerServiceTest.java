d0bae48802b3398db3a5102f09ad4c01b65ee84a
DanielaMarjina <daniela.marjina@student.usv.ro>
2026-09-07 14:48:49 +0300

    test(service):Added the happy path and the error path for findByEmail in Owner Service

--- src/test/java/com/danielamarjina/carinsurance/service/OwnerServiceTest.java
+++ src/test/java/com/danielamarjina/carinsurance/service/OwnerServiceTest.java
@@ -3,6 +3,7 @@ package com.danielamarjina.carinsurance.service;
 import com.danielamarjina.carinsurance.dto.request.OwnerRequest;
 import com.danielamarjina.carinsurance.dto.response.OwnerResponse;
 import com.danielamarjina.carinsurance.entity.Owner;
+import com.danielamarjina.carinsurance.exception.OwnerNotFoundException;
 import com.danielamarjina.carinsurance.mapper.OwnerMapper;
 import com.danielamarjina.carinsurance.repository.OwnerRepository;
 import org.junit.jupiter.api.Test;
@@ -10,6 +11,8 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.verify;
@@ -52,5 +55,38 @@ class OwnerServiceTest {
         verify(ownerMapper).toEntity(ownerRequest);
         verify(ownerRepository).save(owner);
         verify(ownerMapper).toResponse(owner);
+    }
+
+    @Test
+    void findOwnerByEmail_shouldReturnOwner_whenOwnerExists(){
+        String email="test@gmail.com";
+        Owner owner=new Owner();
+        OwnerResponse ownerResponse=new OwnerResponse();
+
+        when(ownerRepository.findByEmail(email))
+                .thenReturn(Optional.of(owner));
+
+        when(ownerMapper.toResponse(owner))
+                .thenReturn(ownerResponse);
+
+        OwnerResponse result=ownerService.findOwnerByEmail(email);
+
+        assertEquals(ownerResponse,result);
+
+        verify(ownerRepository).findByEmail(email);
+        verify(ownerMapper).toResponse(owner);
+    }
+
+    @Test
+    void findOwnerByEmail_shouldThrowException_whenOwnerDoesNotExist(){
+        String email="test@gmail.com";
+
+        when(ownerRepository.findByEmail(email))
+                .thenReturn(Optional.empty());
+
+        assertThrows(OwnerNotFoundException.class,
+                ()->ownerService.findOwnerByEmail(email));
+
+        verify(ownerRepository).findByEmail(email);
     }
 }
